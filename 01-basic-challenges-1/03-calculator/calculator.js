e5fceaa250307859ac20438d6642a062de9c61ef
JemoSurman <jemosurman@gmail.com>
2024-03-24 21:06:07 +0400

    third commit

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,40 @@
-function calculator() {}
+// function calculator(num1, num2, operator) {
+//     let result;
+
+//     switch (operator) {
+//        case '+':
+//        result = num1 + num2;
+//        break;
+//        case '-':
+//         result = num1 - num2;
+//         break;
+//         case'*':
+//         result = num1 * num2;
+//         break;
+//         case'/':
+//         result = num1 / num2;
+//         break;
+//       default:
+//         throw new Error('invalid operator');
+//     }
+
+//     return result;
+// }
+function calculator (num1, num2, operator) {
+    let result;
+    if(operator === '+') {
+        result = num1 + num2;
+    }else if(operator === '-'){
+        result = num1 - num2;
+    }else if(operator === '*'){
+        result = num1 * num2;
+    }else if(operator === "/"){
+        result = num1 / num2;
+    }else {
+        throw new Error('invalid Operator');
+    }
+
+    return result;
+}
 
 module.exports = calculator;
