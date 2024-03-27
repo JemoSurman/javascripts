32b8162819f2a17197b706619dfa6164f81d4495
JemoSurman <jemosurman@gmail.com>
2024-03-28 00:52:44 +0400

    remove-duplicate

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,20 @@
-function removeDuplicates() {}
+//ამოხსნა პირველი
+// function removeDuplicates(arr) {
+//     const uniqueArr = [];
+
+//     for(let i = 0; i < arr.length; i++){
+//         if (!uniqueArr.includes(arr[i])) {
+//            uniqueArr.push(arr[i]);
+//         }
+//     }
+//     return uniqueArr;
+// }
+
+//ამოხსნა მეორე
+
+function removeDuplicates(arr) {
+    
+    return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;
