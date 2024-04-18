242bda2f2487c8d5d130a41c1315e349498bc103
JemoSurman <jemosurman@gmail.com>
2024-04-18 22:00:15 +0400

    find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if(!arr || arr.length === 0){
+        return undefined;
+
+    }else{
+
+        const n = arr.length + 1;
+        const expectedSum = (n*(n + 1))/2;
+
+        let actualSum = 0; 
+        for(let i = 0; i < arr.length; i++){
+            actualSum += arr[i];
+        }    
+        
+        return expectedSum - actualSum;
+    }
+    
+}
 
 module.exports = findMissingNumber;
