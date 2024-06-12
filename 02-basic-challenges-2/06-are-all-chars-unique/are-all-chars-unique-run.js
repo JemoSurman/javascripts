2e4fef3c4f1175201aff2aec91e9308e572afb19
JemoSurman <jemosurman@gmail.com>
2024-06-12 17:38:56 +0400

    areAllcharsUnique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
@@ -5,5 +5,5 @@ const result2 = areAllCharactersUnique('abcdefgA');
 const result3 = areAllCharactersUnique('programming');
 
 console.log(result1);
-console.log(result2);
-console.log(result3);
+// console.log(result2);
+// console.log(result3);
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,30 @@
-function areAllCharactersUnique() {}
+// solution 1
+// function areAllCharactersUnique(str) {
+//     const charSet = new Set();
+
+//     for(let i = 0; i < str.length; i++){
+//         const char = str[i];
+//         if(charSet.has(char)){
+//             return false;
+//         }
+//         charSet.add(char);
+//     }
+//     return true;
+
+// }
+// solution 2
+function areAllCharactersUnique(str){
+    const charCount = {};
+    for (let i = 0; i < str.length; i++){
+        const char = str[i];
+        if(charCount[char]){
+            return false;
+        }
+        charCount[char] = true;
+        // console.log(charCount);
+    }
+    return true;
+}
+
 
 module.exports = areAllCharactersUnique;
