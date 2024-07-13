d882f62e0e08842003003c51c89d8086765e8707
JemoSurman <jemosurman@gmail.com>
2024-07-13 16:11:32 +0400

    Format-phone-number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,20 @@
-function formatPhoneNumber() {}
+// function formatPhoneNumber(numbers) {
+//     const areacode = numbers.slice(0,3).join("");
+//     const prefix = numbers.slice(3, 6).join("");
+//     const lineNumber = numbers.slice(6).join("");
+
+//     return `(${areacode}) ${prefix}-${lineNumber}`;
+
+// }
+
+// function formatPhoneNumber(numbers) {
+//     const formatted = numbers.join("");
+//     return `(${formatted.slice(0, 3)}) ${formatted.slice(3, 6)}-${formatted.slice(6)}`;
+
+// }
+
+const formatPhoneNumber = (numbers) => {
+    return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
+}
 
 module.exports = formatPhoneNumber;
