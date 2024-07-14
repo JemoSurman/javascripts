d83f01df85a8bd02ed11111b0156c2b350d28968
JemoSurman <jemosurman@gmail.com>
2024-07-14 13:24:41 +0400

    sum-of-squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,14 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+    const sumOfEvenSquares = numbers
+
+    .filter((num) => num % 2 === 0)
+
+    .map((num) => num * num)
+
+    .reduce((sum, square) => sum + square, 0)
+
+    return sumOfEvenSquares;
+}
+
 
 module.exports = sumOfEvenSquares;
