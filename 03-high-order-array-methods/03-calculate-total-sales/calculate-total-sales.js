db8169539197ef20e02f54955d000c9558bc5ef3
JemoSurman <jemosurman@gmail.com>
2024-07-16 18:56:26 +0400

    calculate-total-sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,12 +1,13 @@
 function calculateTotalSalesWithTax(products, taxRate) {
     const totalSales = products.reduce(
+        
     (sum, products) => sum + products.price * products.quantity, 0
 )
     const taxAmount = (totalSales * taxRate) / 100;
 
     const totalSalesWithTax = totalSales + taxAmount;
 
-    return parseFloat(totalSalesWithTax.toFixed(2));
+    return parseFloat(totalSalesWithTax).toFixed(2);
 
 }
 
