9aac40c6cc809585593267c6974a1e7178c874b7
JemoSurman <jemosurman@gmail.com>
2024-09-07 17:39:33 +0400

    valid-IPv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
@@ -1,6 +1,6 @@
 const isValidIPv4 = require('./valid-ipv4.js');
 
-const result1 = isValidIPv4('122.164.23.21');
-const result2 = isValidIPv4('122.164.23.21.33');
+const result1 = isValidIPv4('122.164.33.21');
+const result2 = isValidIPv4('122.165.23.22.21');
 
 console.log(result1, result2);
