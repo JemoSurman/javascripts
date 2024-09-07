a9788b7ec8cd598dc5d92b80818822b4df951c53
JemoSurman <jemosurman@gmail.com>
2024-09-07 15:00:55 +0400

    hashtag-generator2

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,16 +1,28 @@
-function generateHashtag(str) {
-    if (str.trim() === ''){
-        return false;
-    }
+// function generateHashtag(str) {
+//     if (str.trim() === ''){
+//         return false;
+//     }
 
-    const words = str.trim().split(/\s+/);
+//     const words = str.trim().split(/\s+/);
     
-    const captilazedWords = words.map((word) => word.charAt(0).toUpperCase() + 
-    word.slice(1));
+//     const captilazedWords = words.map((word) => word.charAt(0).toUpperCase() + 
+//     word.slice(1));
     
-    const hashtag = '#' + captilazedWords.join('');
+//     const hashtag = '#' + captilazedWords.join('');
 
-    return hashtag.length > 140 ? false : hashtag;
+//     return hashtag.length > 140 ? false : hashtag;
+// }
+
+function generateHashtag(str){
+    const hashtag = str.split(' ').reduce(function (tag, word) {
+        return tag + word.charAt(0).toUpperCase() + word.slice(1);
+    }, '#')
+    
+    return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
 }
+
+// The result of this step is '#' + 'H' + 'ello' = "#Hello".
+    
+
 
 module.exports = generateHashtag;
