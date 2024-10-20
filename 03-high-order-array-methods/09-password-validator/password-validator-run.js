1de10febfc327eaf49105890663e80acbbccb449
JemoSurman <jemosurman@gmail.com>
2024-10-20 13:00:59 +0400

    password-validation

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -1,6 +1,6 @@
 const validatePassword = require('./password-validator');
 
-const result1 = validatePassword('Abc12345');
+const result1 = validatePassword('Abc123435');
 const result2 = validatePassword('password');
 
-console.log(result1, result2);
+console.log(result1);
