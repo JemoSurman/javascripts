905e93c0e290a269a834fca1a7785c9096012d74
JemoSurman <jemosurman@gmail.com>
2025-02-16 00:22:43 +0400

    simple-examples1

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -3,27 +3,29 @@ const numbers = [1, 2, 3, 4, 5];
 /**
  * map: Transforms array elements with a provided function, creating a new array.
  */
+
 const devideArr = numbers.map((num) => {
     return num/2;
-});
+})
 // console.log(devideArr);
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
 const oddNumbers = numbers.filter((num) => {
-   return num % 2 === 1; 
-});
+    return num % 2 == 1;
+})
 // console.log(oddNumbers);
 
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
+
 const multiply = numbers.reduce((total, num) => {
     return total * num;
 });
-// console.log(multiply);
+console.log(multiply);
 
 
 /**
@@ -58,5 +60,5 @@ const some = numbers.some((num) => {
 const every = numbers.every((num) => {
     return num % 2 === 1;
 });
-console.log(every);
+// console.log(every);
 
