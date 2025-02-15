c8d478b03b266e6cbc898d89cd5e421a24051a18
JemoSurman <jemosurman@gmail.com>
2025-02-16 00:14:27 +0400

    example

--- 03-high-order-array-methods/01-simple-examples/simple-examples-done.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples-done.js
@@ -3,24 +3,22 @@ const numbers = [1, 2, 3, 4, 5];
 /**
  * map: Transforms array elements with a provided function, creating a new array.
  */
-const doubleNumbers - numbers.map((num) => num * 2);
+const doubleNumbers = numbers.map((num) => num * 2);
 
-// console.log(doubledNumbers);
+// console.log(doubleNumbers);
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
 
 const evenNumbers = numbers.filter((num) => num > 2);
-
-// console.log(evenNumbers);
+//  console.log(evenNumbers);
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
 
-const sum = numbers.reduce((total, num) => total + num, 0);
-
+const sum = numbers.reduce((total, num) => total + num, 0)
 // console.log(sum);
 
 /**
@@ -33,15 +31,14 @@ const sum = numbers.reduce((total, num) => total + num, 0);
  * find: Returns the first array element that satisfies a specified condition.
  */
 
-const foundNumber = numbers.find((num) => num > 2);
-
+const foundNumber = numbers.find((num) => num < 5);
 // console.log(foundNumber);
 
 /**
  * some: Checks if at least one array element satisfies a condition.
  */
 
-const hasEvenNumber = numbers.some((num) => num > 5);
+const hasEvenNumber = numbers.some((num) => num > 2);
 
 // console.log(hasEvenNumber);
 
@@ -49,6 +46,6 @@ const hasEvenNumber = numbers.some((num) => num > 5);
  * every: Checks if all array elements satisfy a condition.
  */
 
-const allNumsGreaterThanZero = numbers.every((num) => num > 0);
+const allNumsGreaterThanZero = numbers.every((num) => num > 3);
 
 console.log(allNumsGreaterThanZero);
