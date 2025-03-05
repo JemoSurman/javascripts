f4375dc8f90e50ad43b7d9bbd585f2095fb41f7c
JemoSurman <jemosurman@gmail.com>
2025-03-06 03:31:34 +0400

    change-solution-IPV4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-solution.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-solution.js
@@ -1,15 +1,16 @@
-const isValidIPv4 = (input) => {
-  // Split the input string into an array of octets.
-  const octets = input.split('.');
-  // If the input string does not contain exactly 4 octets, return false.
-  if (octets.length !== 4) {
+var str = "2.1.1.3"
+
+function validIpv4(str) {
+  if (str.length < 7 || str.length > 14) {
     return false;
   }
-  // Otherwise, return true if every octet is a number between 0 and 255.
-  return octets.every((octet) => {
-    const num = parseInt(octet); // Convert the octet to a number.
-    return num >= 0 && num <= 255 && octet === num.toString(); // Return true if the octet is between 0 and 255 and the octet is equal to the number converted back to a string.
-  });
-};
+
+  const removePoint = str.split(".");
+  const valid = removePoint.every((num) => num > 0 && num <= 255);
+
+  return valid;
+}
+
+console.log(validIpv4(str));
 
 module.exports = isValidIPv4;
---
+++ New Text Document.js
@@ -0,0 +1,9 @@
+const str = "  mamul davitadze   ";
+let result = str.trim();
+console.log(result);
+function generatedHashtag(str){
+    if(str.trim() === ''){
+        return false;
+    }
+    
+}
