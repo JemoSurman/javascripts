45abedce7dd459ae19ce6b3ba0103c8759bfbd22
JemoSurman <jemosurman@gmail.com>
2025-03-07 00:58:27 +0400

    analyze-car-mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,27 +1,21 @@
 function analyzeCarMileage(cars) {
+    const totalMileage = cars.reduce((sum, car) => sum + car.mileage, 0);
+    const averageMileage = totalMileage / cars.length;
+    const highestMileageCar = cars.reduce(
+        (highest, car) => (car.mileage > highest.mileage ? car : highest),
+        cars[0]
+    );
+    const lowestMileageCar = cars.reduce(
+        (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+        cars[0]
+    );
 
-    const totalMileage = cars.reduce((sum, car) => {
-        return sum + car.mileage;
-    },0)
-
-    const averageMileage = totalMileage / cars.length
-
-    const highestMileageCar = cars.reduce((highest, car) => {
-        return (car.mileage > highest.mileage ? car : highest)
-    }, cars[0])
-
-
-    const lowestMileageCar = cars.reduce((lowest, car) => {
-        return (car.mileage < lowest.mileage ? car : lowest) 
-    }, cars[0])
-
-    
-   return{averageMileage: parseFloat(averageMileage.toFixed(2)),
-      highestMileageCar,
-      lowestMileageCar,       
-      totalMileage
-   };
-
+    return {
+        averageMileage: parseFloat(averageMileage.toFixed(2)),
+        highestMileageCar,
+        lowestMileageCar,
+        totalMileage,
+    };
 }
 
 module.exports = analyzeCarMileage;
