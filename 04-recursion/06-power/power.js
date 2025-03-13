09f31f5d16c9ba5724e0f34cd29552004c421a95
JemoSurman <jemosurman@gmail.com>
2025-03-13 16:06:42 +0400

    power

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,11 @@
-function power() {}
+function power(num,power1) {
+    if(power1 === 0){
+        return 1;
+    }else{
+        return num * power(num, power1 - 1);
+    }
+}
+
+    
 
 module.exports = power;
