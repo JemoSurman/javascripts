ce77e15ce041eae2bf07b24ca85a32ddb2892671
JemoSurman <jemosurman@gmail.com>
2025-03-19 01:35:33 +0400

    constant0time

--- 05-complexity/03-constant-time-complexity/constant-time.js
+++ 05-complexity/03-constant-time-complexity/constant-time.js
@@ -3,3 +3,10 @@
 
   Constant time means that the time required to complete a function is the same regardless of the size of the input data set.
 */
+
+function accessElement(arr, index) {
+  return arr[index];
+}
+
+const arr1 = [1,2,3];
+console.log(arr1, 2);
