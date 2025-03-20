b08754439625d470042f2ff702771b4341b50bc1
JemoSurman <jemosurman@gmail.com>
2025-03-20 19:56:23 +0400

    constant-time

--- 05-complexity/03-constant-time-complexity/constant-time.js
+++ 05-complexity/03-constant-time-complexity/constant-time.js
@@ -8,5 +8,13 @@ function accessElement(arr, index) {
   return arr[index];
 }
 
-const arr1 = [1,2,3];
-console.log(arr1, 2);
+const arr1 = [1,2,3,4,5];
+console.time('Access Element 1');
+console.log(accessElement(arr1, 1));
+console.timeEnd('Access Element 1');
+console.log(arr1, 1);
+
+const arr2 = Array.from({length : 1000}, (_, index) => index + 1);
+console.time('Access Element 2');
+console.log(accessElement(arr2, 100));
+console.timeEnd('Access Element 2');
