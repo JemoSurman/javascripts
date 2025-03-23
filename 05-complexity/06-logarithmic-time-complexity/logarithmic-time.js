de69051514ad4c733bc4c212258b0e05287c790b
JemoSurman <jemosurman@gmail.com>
2025-03-23 22:02:14 +0400

    logarithmic-time

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -4,4 +4,24 @@ Logarithmic Time (O(log n))
 Logarithmic time means that the time required to complete a function is proportional to the logarithm of the input data set. 
 
 */
+function findPower(base, exponent){
+    if(exponent === 0){
+        return 1;
+    }
 
+    if(exponent % 2 === 0){
+        const halfPower = findPower(base, exponent / 2);
+        return halfPower * halfPower;
+    }else {
+        const halfPower = findPower(base, (exponent - 1) / 2);
+        return base * halfPower * halfPower;
+    }
+}
+
+console.time("Find Power 1");
+console.log(findPower(2, 100));
+console.timeEnd("Find Power 1");
+
+console.time("Find Power 2");
+console.log(2, 100000);
+console.timeEnd("Find Power 2");
