f31494e6169b6302dbe301baa12841bffb916cdf
JemoSurman <jemosurman@gmail.com>
2025-03-28 20:39:13 +0400

    max-sumarray

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,14 +1,15 @@
 function maxSubarraySum(arr, k) {
-    let maxSum = 0;
+    let maxSum = -Infinity;
 
-    for (let i = 0; i <= arr.length - k; i++) {
+    for(let i = 0; i <= arr.length - k; i++){
         let currentSum = 0;
 
-        for (let j = i; j < i + k; j++) {
+        for(let j = i; j < i + k; j++){
             currentSum += arr[j];
-        }
 
+        }
         maxSum = Math.max(maxSum, currentSum);
+        
     }
 
     return maxSum;
