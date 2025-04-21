a24bcda2241a3d2b5edcde9b059f7e9ca50a39e7
JemoSurman <jemosurman@gmail.com>
2025-04-21 18:48:16 +0400

    maps-done

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,24 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+    let maxSum = 0;
+    let currentSum = 0;
 
+    for(let i = 0; i < k; i++){
+        maxSum += arr[i];
+    }
+    
+    currentSum = maxSum;
+
+    for(let i = k; i < arr.length; i++){
+        currentSum = currentSum - arr[i - k] + arr[i];
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4];
+console.log(maxSubarraySum(arr2, 4))
+//2 3 4 5 6
+//2
+//2 + 3 -> 5
+//3 4
 module.exports = maxSubarraySum;
