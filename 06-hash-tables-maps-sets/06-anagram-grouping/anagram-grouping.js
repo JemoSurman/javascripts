5987c54dfea1119fe234f35493f32aca76aac224
JemoSurman <jemosurman@gmail.com>
2025-04-24 19:40:58 +0400

    anagram-grouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,19 @@
-function anagramGrouping() {}
+function anagramGrouping(words) {
+    const anagramGroups = new Map();
+
+    for(const word of words){
+        const sortedChars = word.split('').sort().join('');
+        
+        if(!anagramGroups.has(sortedChars)){
+            angramGroups.set(sortedChars, [word]);
+        }else {
+            anagramGroups.get(sortedChars).push(word);
+        } 
+    }
+
+    return Array.from(anagramGroups.values());
+}
+
+
 
 module.exports = anagramGrouping;
