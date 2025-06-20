3aff319101ea335747fa4efdd9bbb8585af3b2a0
JemoSurman <jemosurman@gmail.com>
2025-06-20 19:08:41 +0400

    two-sum

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,17 @@
-function twoSum() {}
+function twoSum(nums, target) {
+    const set = new Set();
+
+    for(let i = 0; i < nums.length; i++){
+        const complement = target - nums[i];
+
+        if(set.has(complement)){
+            return [nums.indexOf(complement), i];
+        }
+
+        set.add(nums[i]);
+    }
+
+    return []; 
+}
 
 module.exports = twoSum;
