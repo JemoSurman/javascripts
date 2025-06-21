c4b6140eaa04d031593a420f19d7d0d2ede62d40
JemoSurman <jemosurman@gmail.com>
2025-06-21 15:46:29 +0400

    custom-hash-table

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,13 @@
 const HashTable = require('./custom-hash-table');
+
+const myHashTable = new HashTable();
+
+const result = myHashTable._hash('John',myHashTable.limit);
+
+//console.log(result);
+
+myHashTable.set('John','555-321-568');
+myHashTable.set('Jane', '555-121-788');
+myHashTable.set('Sara', '555-482-923');
+
+myHashTable.printTable();
