928a2eef37f008bb69d518c4e3892ce7b41e01da
JemoSurman <jemosurman@gmail.com>
2025-08-20 15:48:31 +0400

    custom0anagram-grouping

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,18 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+    const anagramGrouping = new HashTable();
+
+    for(const word of words){
+        const sortedChars = word.split('').sort().join('');
+
+        if(anagramGrouping.get(sortedChars)){
+            anagramGrouping.get(sortedChars).push(word);
+        }else {
+            anagramGrouping.set(sortedChars, [word]);
+        }
+    }
+    return anagramGrouping.getValues();
+}
 
 module.exports = anagramGrouping;
