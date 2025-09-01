7c07ced9180697853f0d591ba7c435296d031f0a
JemoSurman <jemosurman@gmail.com>
2025-09-01 15:18:41 +0400

    queue

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,42 @@
-class Queue {}
+class Queue {
+    constructor(){
+        this.queue = [];
+        this.head = 0;
+        this.tail = 0;
+        this.maxSize = 100;
+    }
+
+    enqueue(item){
+        if(this.isFull()){
+            return false;
+        }
+
+        this.queue[this.tail] = item;
+        this.tail++;
+        return true;
+    }
+
+    getLength(){
+        return this.tail - this.head;
+    }
+
+    isFull(){
+        return this.getLength() === this.maxSize;
+    }
+
+    peek(){
+        return this.queue[this.head];
+    }
+
+    isEmpty(){
+        return this.getLength() === 0;
+    }
+
+    dequeue(){
+        const item = this.queue[this.head];
+        this.head++;
+        return item;
+    }
+}
 
 module.exports = Queue;
