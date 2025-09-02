7e94df80d0d5cbe61d9f5c37cfcba35671769d46
JemoSurman <jemosurman@gmail.com>
2025-09-02 11:13:55 +0400

    palindrome-queue-stack

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack-run.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack-run.js
@@ -1,6 +1,6 @@
 const isPalindromeQueueStack = require('./palindrome-queue-stack');
 
 const result1 = isPalindromeQueueStack('A man, a plan, a canal: Panama');
-const result2 = isPalindromeQueueStack('Hello');
+const result2 = isPalindromeQueueStack('ana');
 
 console.log(result1, result2);
--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,24 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+    const formattedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const queue = new Queue();
+    const stack = new Stack();
+    
+    
+    for(let i = 0; i < formattedStr.length; i++){
+        stack.push(formattedStr[i]);
+        queue.enqueue(formattedStr[i])
+    }
+
+    for(let i = 0; i < formattedStr.length; i++){
+        if(stack.pop() !== queue.dequeue()){
+            return false;
+        }
+    }
+    
+    return true;
+}
 
 module.exports = isPalindromeQueueStack;
