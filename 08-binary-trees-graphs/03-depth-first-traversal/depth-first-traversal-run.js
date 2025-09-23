16508f492429f96fef9cd4d5e0d6f1b3de8aa93a
JemoSurman <jemosurman@gmail.com>
2025-09-23 23:03:01 +0400

    depth-first-traversal

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
@@ -5,4 +5,19 @@ const { Node, depthFirstTraversal } = require('./depth-first-traversal');
 //                         b     c
 //                        / \    /
 //                       d   e  f
+const root = new Node('a');
+const NodeB = new Node('b');
+const NodeC = new Node('c');
+const NodeD = new Node('d');
+const NodeE = new Node('e');
+const NodeF = new Node('f');
 
+root.left = NodeB;
+root.right = NodeC;
+NodeB.right = NodeE;
+NodeB.left = NodeD;
+NodeC.left = NodeF;
+
+const result = depthFirstTraversal(root);
+
+console.log(result);
