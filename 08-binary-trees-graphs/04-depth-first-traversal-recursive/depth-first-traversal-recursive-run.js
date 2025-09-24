d57f652aff7f53f8186ab37395fc571b7282933b
JemoSurman <jemosurman@gmail.com>
2025-09-24 22:09:38 +0400

    depth-first-traversal-recursive

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive-run.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive-run.js
@@ -1,8 +1,24 @@
-const { Node, depthFirstTraversal } = require('./depth-first-traversal');
+const { Node, recDepthFirstTraversal } = require('./depth-first-traversal-recursive');
 
 // Create a binary tree:      a
 //                          /   \
 //                         b     c
 //                        / \    /
 //                       d   e  f
+const root = new Node('a');
+const NodeB = new Node('b');
+const NodeC = new Node('c');
+const NodeD = new Node('d');
+const NodeE = new Node('e');
+const NodeF = new Node('f');
+
+root.left = NodeB;
+root.right = NodeC;
+NodeB.right = NodeE;
+NodeB.left = NodeD;
+NodeC.left = NodeF;
+
+const result = recDepthFirstTraversal(root);
+
+console.log(result);
 
