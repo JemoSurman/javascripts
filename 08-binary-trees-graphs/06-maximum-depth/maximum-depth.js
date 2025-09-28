64ec1ee82aa3184a4b6f8ece4f3466c6c6e7054d
JemoSurman <jemosurman@gmail.com>
2025-09-28 12:00:19 +0400

    maximum-depth

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,19 @@
-class Node {}
+class Node {
+  constructor(value){
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+  if(!root) return [];
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+}
 
 module.exports = {
   maxDepth,
