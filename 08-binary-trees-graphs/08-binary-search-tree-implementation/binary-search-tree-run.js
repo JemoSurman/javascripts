bccab5353632079ce5c0c13c3480bcf7d359522b
JemoSurman <jemosurman@gmail.com>
2025-09-28 17:08:40 +0400

    binary-search-tree

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -7,3 +7,11 @@ const { Node, BinarySearchTree } = require('./binary-search-tree');
 //    /
 //   2
 
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(15);
+
+bst.printTree();
